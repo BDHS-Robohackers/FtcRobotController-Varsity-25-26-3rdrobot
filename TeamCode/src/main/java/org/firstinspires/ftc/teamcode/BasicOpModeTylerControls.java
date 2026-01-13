86532984a47f720e02eee69fe6875a40f7ae88cc
Carter Rommelfanger <contact@cookiejar499.me>
2026-01-12 20:39:36 -0600

    Almost done calibrating PID loop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpModeTylerControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpModeTylerControls.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -28,13 +29,15 @@ public class BasicOpModeTylerControls extends LinearOpMode {
     private double lateral = 0;
     private double yaw = 0;
 
-    public static double TARGET_FLY_SPEED = 360; // Degrees per second
+    // 200 - MAX
+    public static double TARGET_FLY_SPEED = 195; // Degrees per second
+    public static double TARGET_FLY_SPEED_THRESH = 13; // Degrees per second
 
-    public static double flyKP = 0.01; // Proportion
-    public static double flyKI = 0.0; // Integral
-    public static double flyKD = 0.0; // Derivative
+    public static double flyKP = 0.10; // Proportion
+    public static double flyKI = 0.0011; // Integral
+    public static double flyKD = 0.0009; // Derivative
 
-    public static double flyKF = 0.0001; // Feed forward; Find by taking POWER/SPEED
+    public static double flyKF = 0.001; // Feed forward; Find by taking POWER/SPEED
 
     public static double MAX_INTEGRAL = 5000.0; // May need to adjust as needed
     public static double MIN_INTEGRAL = -5000.0; // May need to adjust as needed
@@ -48,11 +51,12 @@ public class BasicOpModeTylerControls extends LinearOpMode {
      * Updates the fly wheel based on a simple PID loop
      */
     public void flyWheelLoop() {
-        if ((intervalTime.seconds() > 2) || resetPidTimer) {
+        if ((intervalTime.milliseconds() > 2000) || resetPidTimer) {
             resetPidTimer = false;
             intervalTime.reset(); // Too much time has elapsed; reset timer and wait for next loop.
             return;
         }
+
         final double dt = intervalTime.milliseconds() / 1000.0; // Last Interval time in seconds
         final double speed = ((DcMotorEx) robot.fly).getVelocity(AngleUnit.DEGREES);
 
@@ -68,7 +72,11 @@ public class BasicOpModeTylerControls extends LinearOpMode {
 
         final double output = Math.max(-1.0, Math.min(1.0, rawOutput));
 
-        robot.fly.setPower(output);
+        telemetry.addData("Fly Power: ", output);
+        telemetry.addData("Raw Power: ", rawOutput);
+        telemetry.addData("Interval: ", dt);
+
+        robot.updateFlywheelMotorsOverrideMax(output);
 
         flySpeedErrorPrev = error;
 
@@ -81,6 +89,7 @@ public class BasicOpModeTylerControls extends LinearOpMode {
         otherController = gamepad2;
         robot = new Robot();
         robot.initialize(hardwareMap);
+        robot.fly.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Wait for the robot to start (driver presses PLAY).
         telemetry.addData("Status", "Initialized.");
@@ -108,6 +117,9 @@ public class BasicOpModeTylerControls extends LinearOpMode {
             telemetry.addData("Yaw (rotate) %", yaw);
             telemetry.addData("Axial (FW/RV) %", axial);
             telemetry.addData("Lateral (Strafe) %", lateral);
+            telemetry.addData("Integral: ", flyIntegral);
+            telemetry.addData("Fly Vel (Deg/s): ", ((DcMotorEx) robot.fly).getVelocity(AngleUnit.DEGREES));
+            telemetry.addData("Error prev: ", flySpeedErrorPrev);
             // Update Ethan servo control based on D-pad input
 
             telemetry.update();
@@ -129,7 +141,6 @@ public class BasicOpModeTylerControls extends LinearOpMode {
         // variable control based on buttons pushed
         if (otherController.right_bumper) {
             flywheelControl = 0;
-            resetPidTimer = true;
         } else if (otherController.x) {
             flywheelControl = 0.75;
         } else if (otherController.b) {
@@ -149,17 +160,23 @@ public class BasicOpModeTylerControls extends LinearOpMode {
         }
 
         isYPressed = otherController.y;
+        final double motorSpeed = ((DcMotorEx) robot.fly).getVelocity(AngleUnit.DEGREES);
+        boolean canShoot = motorSpeed > (TARGET_FLY_SPEED - TARGET_FLY_SPEED_THRESH);
 
-        if (isYPressed) {
+        if (isYPressed && canShoot) {
             robot.updateFlyFeedMotor(1);
         } else {
             robot.updateFlyFeedMotor(0);
         }
 
         if (flywheelControl != 0) {
-            flyWheelLoop(); // Variable speed not needed anymore
-//            robot.updateFlywheelMotors(flywheelControl);  // variable speed on flywheel depending on button
+            if (flywheelControl < 0.9) {
+                robot.updateFlywheelMotors(flywheelControl);  // variable speed on flywheel depending on button
+            } else {
+                flyWheelLoop(); // Variable speed not needed anymore
+            }
         } else {
+            resetPidTimer = true;
             robot.updateFlyFeedMotor(0.0f);
             robot.updateFlywheelMotors(0.0f);  // Stop flywheel
         }
