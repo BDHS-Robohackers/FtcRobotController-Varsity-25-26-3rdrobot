eff9b2ec09b266cbc15a3da9aa4aa9ea7c8ca79b
Carter Rommelfanger <contact@cookiejar499.me>
2026-01-25 18:58:32 -0600

    Added pid to auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedFarAuto6EncodersRev3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedFarAuto6EncodersRev3.java
@@ -2,7 +2,10 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Robot;
 
 import java.util.ArrayList;
@@ -24,6 +27,59 @@ public class RedFarAuto6EncodersRev3 extends LinearOpMode {
 
     private static final double SPEED = 0.85;
 
+    // 200 - MAX
+    private final ElapsedTime intervalTime = new ElapsedTime();
+    public static double TARGET_FLY_SPEED = 195; // Degrees per second
+    public static double TARGET_FLY_SPEED_THRESH = 13; // Degrees per second
+
+    public static double flyKP = 0.10; // Proportion
+    public static double flyKI = 0.0011; // Integral
+    public static double flyKD = 0.0009; // Derivative
+
+    public static double flyKF = 0.001; // Feed forward; Find by taking POWER/SPEED
+
+    public static double MAX_INTEGRAL = 5000.0; // May need to adjust as needed
+    public static double MIN_INTEGRAL = -5000.0; // May need to adjust as needed
+
+    public double flySpeedErrorPrev = 0.0;
+    public double flyIntegral = 0.0;
+    public double flySpeedModifier = 0.0f;
+
+    private boolean resetPidTimer = false;
+
+    public void flyWheelLoop() {
+        if ((intervalTime.milliseconds() > 2000) || resetPidTimer) {
+            resetPidTimer = false;
+            intervalTime.reset(); // Too much time has elapsed; reset timer and wait for next loop.
+            return;
+        }
+
+        final double dt = intervalTime.milliseconds() / 1000.0; // Last Interval time in seconds
+        final double speed = ((DcMotorEx) robot.fly).getVelocity(AngleUnit.DEGREES);
+
+        final double error = TARGET_FLY_SPEED - speed;
+
+        final double feedForward = flyKF * TARGET_FLY_SPEED;
+
+        flyIntegral += error * dt;
+        flyIntegral = Math.max(MIN_INTEGRAL, Math.min(MAX_INTEGRAL, flyIntegral));
+
+        final double derivative = (error - flySpeedErrorPrev) / dt;
+        final double rawOutput = feedForward + (flyKP * error) + (flyKI * flyIntegral) + (flyKD * derivative);
+
+        final double output = Math.max(0, Math.min(1.0, rawOutput)); // Min changed to 0 instead of -1
+
+        telemetry.addData("Fly Power: ", output);
+        telemetry.addData("Raw Power: ", rawOutput);
+        telemetry.addData("Interval: ", dt);
+
+        robot.updateFlywheelMotorsOverrideMax(output);
+
+        flySpeedErrorPrev = error;
+
+        intervalTime.reset(); // Reset timer to get interval time
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -40,14 +96,14 @@ public class RedFarAuto6EncodersRev3 extends LinearOpMode {
         List<Runnable> plan = new ArrayList<>();
 
         // Spin up flywheel
-        plan.add(this::spinUp);
+        // plan.add(this::spinUp);
         plan.add(() -> robot.updateFrontIntakeMotors(1));
 
         // === don't BACK UP ===
-        plan.add(() -> robot.updateFlywheelMotors(-0.95));
+//        plan.add(() -> robot.updateFlywheelMotors(-0.95));
         //plan.add(() -> robot.driveForwardEncoder(-35, SPEED));
         //plan.add(() -> robot.driveForwardEncoder(0.5,SPEED));
-        plan.add(() -> robot.updateFlywheelMotors(-0.82));
+//        plan.add(() -> robot.updateFlywheelMotors(-0.82));
         plan.add(() -> sleep(500));
 
         // === SHOOT 3 BALLS ===
@@ -120,6 +176,7 @@ public class RedFarAuto6EncodersRev3 extends LinearOpMode {
         // run actions sequentially while updating flywheel continuously
         for (Runnable action : plan) {
             if (!opModeIsActive()) break;
+            flyWheelLoop();
             action.run();
         }
 
@@ -142,8 +199,9 @@ public class RedFarAuto6EncodersRev3 extends LinearOpMode {
         sleep(150);
     }
 
-    private void spinUp() {
-        robot.updateFlywheelMotors(-1);
-        sleep(2700);
-    }
+//    private void spinUp() {
+//        flyWheelLoop();
+//        //robot.updateFlywheelMotors(-1);
+//        sleep(2700);
+//    }
 }
