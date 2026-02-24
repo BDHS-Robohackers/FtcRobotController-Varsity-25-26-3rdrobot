5957546a26e8e394bbbf016416d2be28874c6e1e
wiiuenjoyer36 <159852217+wiiuenjoyer36@users.noreply.github.com>
2026-02-23 19:34:28 -0600

    tweaks with  tuning roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 4.081332041884899;
         public double kV = 0.0026765169252653868;
-        public double kA = 1;
+        public double kA = 0.000001;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,9 +82,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 0.002;
+        public double lateralGain = 0.002;
+        public double headingGain = 0.004; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
